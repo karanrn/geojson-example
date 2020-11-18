2fbcb3e86055c1584ca2ed1b789e31b013a10adf
Karan Nadagoudar <karan4080@gmail.com>
2020-11-18 15:50:57 +0530

    Added webserver, GET for states and union territories

--- app.go
+++ app.go
@@ -6,9 +6,11 @@ https://www.geeksforgeeks.org/find-the-centroid-of-a-non-self-intersecting-close
 
 */
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"sort"
+	"log"
+	"net/http"
 
 	"github.com/paulmach/orb"
 	"github.com/paulmach/orb/geojson"
@@ -20,11 +22,14 @@ const (
 	GEOFILE = "IndianStates.json"
 )
 
+// UTs is union territories of the India
+var UTs = []string{"Andaman and Nicobar", "Chandigarh", "Dadra and Nagar Haveli", "Daman and Diu", "Delhi", "Jammu and Kashmir", "Ladakh", "Lakshadweep", "Puducherry"}
+
+// Loc holds Latitude and Longitude information
 /*
 East to West (Lat) : 98 to 70
 North to South (Lon): 36 to 8
 */
-// Loc holds Latitude and Longitude information
 type Loc struct {
 	Lat float64
 	Lon float64
@@ -33,12 +38,22 @@ type Loc struct {
 // StateCentriod has centriod of the state
 type StateCentriod struct {
 	State    string
+	isUT     bool
 	Centriod Loc
 }
 
+// StateCentriodList slice of type StateCentriod
 type StateCentriodList []StateCentriod
 
-func main() {
+var featureCollections geojson.FeatureCollection
+var sCentriods []StateCentriod
+
+func init() {
+	DataPrep()
+}
+
+// DataPrep loads and prepares data for the API
+func DataPrep() {
 	// Open/load the file
 	f, err := ioutil.ReadFile(GEOFILE)
 	if err != nil {
@@ -47,6 +62,69 @@ func main() {
 	}
 
 	featureCollections, err := geojson.UnmarshalFeatureCollection(f)
+
+	// Find centriods of the state
+	for _, feature := range featureCollections.Features {
+		// Check if it is a UT
+		ut := Contains(feature.Properties["NAME_1"].(string), UTs)
+
+		_, isMulti := feature.Geometry.(orb.MultiPolygon)
+		if isMulti {
+			sCentriods = append(sCentriods, StateCentriod{
+				State:    feature.Properties["NAME_1"].(string),
+				Centriod: getCentroid(feature.Geometry.(orb.MultiPolygon)[0][0]),
+				isUT:     ut,
+			})
+
+		} else {
+			sCentriods = append(sCentriods, StateCentriod{
+				State:    feature.Properties["NAME_1"].(string),
+				Centriod: getCentroid(feature.Geometry.(orb.Polygon)[0]),
+				isUT:     ut,
+			})
+		}
+	}
+}
+
+// ListStatesOnly returns all the states of India
+func ListStatesOnly(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		var states []string
+		for _, sc := range sCentriods {
+			if !sc.isUT {
+				states = append(states, sc.State)
+			}
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(states)
+	} else {
+		fmt.Fprintf(w, "Method not supported")
+	}
+}
+
+// ListStatesAndUT returns all the states of India
+func ListStatesAndUT(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		var states []string
+		for _, sc := range sCentriods {
+			states = append(states, sc.State)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(states)
+	} else {
+		fmt.Fprintf(w, "Method not supported")
+	}
+
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/statesonly", ListStatesOnly)
+	mux.HandleFunc("/states-ut", ListStatesAndUT)
+	fmt.Println("Serving on :9000")
+	log.Fatal(http.ListenAndServe(":9000", mux))
+
 	/*
 		// List all states
 		for _, feature := range featureCollections.Features {
@@ -65,53 +143,35 @@ func main() {
 
 	*/
 
-	// Find centriods of the state
-	var sCentriods []StateCentriod
-	for _, feature := range featureCollections.Features {
-		_, isMulti := feature.Geometry.(orb.MultiPolygon)
-		if isMulti {
-			//fmt.Println(feature.Properties["NAME_1"], feature.Geometry.(orb.MultiPolygon)[0][0])
-			sCentriods = append(sCentriods, StateCentriod{
-				State:    feature.Properties["NAME_1"].(string),
-				Centriod: getCentroid(feature.Geometry.(orb.MultiPolygon)[0][0]),
-			})
+	/*
+		// East to West, ordered alphabetically
+		sort.Slice(sCentriods, func(i, j int) bool {
+			if sCentriods[i].Centriod.Lat > sCentriods[j].Centriod.Lat {
+				return true
+			}
+			if sCentriods[i].Centriod.Lat < sCentriods[j].Centriod.Lat {
+				return false
+			}
+			return sCentriods[i].State < sCentriods[j].State
+		})
+		for _, s := range sCentriods {
+			fmt.Println(s)
+		}
 
-		} else {
-			sCentriods = append(sCentriods, StateCentriod{
-				State:    feature.Properties["NAME_1"].(string),
-				Centriod: getCentroid(feature.Geometry.(orb.Polygon)[0]),
-			})
+		// North to South, ordered alphabetically
+		sort.Slice(sCentriods, func(i, j int) bool {
+			if sCentriods[i].Centriod.Lon > sCentriods[j].Centriod.Lon {
+				return true
+			}
+			if sCentriods[i].Centriod.Lon < sCentriods[j].Centriod.Lon {
+				return false
+			}
+			return sCentriods[i].State < sCentriods[j].State
+		})
+		for _, s := range sCentriods {
+			fmt.Println(s)
 		}
-		//break
-	}
-
-	// East to West, ordered alphabetically
-	sort.Slice(sCentriods, func(i, j int) bool {
-		if sCentriods[i].Centriod.Lat > sCentriods[j].Centriod.Lat {
-			return true
-		}
-		if sCentriods[i].Centriod.Lat < sCentriods[j].Centriod.Lat {
-			return false
-		}
-		return sCentriods[i].State < sCentriods[j].State
-	})
-	for _, s := range sCentriods {
-		fmt.Println(s)
-	}
-
-	// North to South, ordered alphabetically
-	sort.Slice(sCentriods, func(i, j int) bool {
-		if sCentriods[i].Centriod.Lon > sCentriods[j].Centriod.Lon {
-			return true
-		}
-		if sCentriods[i].Centriod.Lon < sCentriods[j].Centriod.Lon {
-			return false
-		}
-		return sCentriods[i].State < sCentriods[j].State
-	})
-	for _, s := range sCentriods {
-		fmt.Println(s)
-	}
+	*/
 }
 
 func getCentroid(or orb.Ring) Loc {
@@ -157,14 +217,12 @@ func isPointInsidePolygon(fc *geojson.FeatureCollection, point orb.Point) string
 	return ""
 }
 
-func (s StateCentriodList) Len() int {
-	return len(s)
-}
-
-func (s StateCentriodList) Less(i, j int) bool {
-	return s[i].Centriod.Lat > s[j].Centriod.Lat
-}
-
-func (s StateCentriodList) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
+// Contains checks if value exists in the list
+func Contains(key string, utList []string) bool {
+	for _, ut := range utList {
+		if key == ut {
+			return true
+		}
+	}
+	return false
 }
